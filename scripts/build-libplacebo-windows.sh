7350935b489f65d6505f339f763eebfd044cf15f
MR49-MH <mr49.mh@users.noreply.github.com>
2026-08-22 00:22:53 +0800

    fix ci: patch libplacebo utils_gen.py for Python 3.13 ElementTree (v7.349.0)

--- scripts/build-libplacebo-windows.sh
+++ scripts/build-libplacebo-windows.sh
@@ -12,6 +12,8 @@ git clone --recursive https://github.com/haasn/libplacebo.git || exit 1
 fi
 cd libplacebo || exit 1
 git checkout $TAG || exit 1
+# Python 3.13 compat: ElementTree no longer accepts an ElementTree where an Element is expected
+sed -i 's/VkXML(ET.parse(xmlfile))/VkXML(ET.parse(xmlfile).getroot())/' src/vulkan/utils_gen.py || exit 1
 git apply "${SCRIPT_DIR}/flatpak/0002-Vulkan-use-16bit-for-p010.patch" | exit 1
 DIR=./build || exit 1
 meson setup --prefix /mingw64 -Dxxhash=disabled $DIR || exit 1
